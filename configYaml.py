29fc252f43b9bf9f65f039bc127166b749d78858
Leesanel <86757739+Leesanel@users.noreply.github.com>
2021-07-27 10:39:19 +0800

    Update configYaml.py

--- configYaml.py
+++ configYaml.py
@@ -10,4 +10,5 @@ current_path = os.path.abspath(os.path.dirname(__file__))
 with open(current_path + '/config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     overrides = config["network_overrides"]["constants"][config["selected_network"]]
+    # 这里的overrides得到是config字典中匹配network_overrides节点下的constants的内容与selected_network下的内容。
     print("overrides:",overrides)
